ba9edb481f5b22f7fc2460dcc296efcda7e843e2
andela-batolagbe <atolagbe.bisoye@andela.co>
2016-01-07 13:37:41 +0100

    add email filed to user model, redifine relations

--- events/models.py
+++ events/models.py
@@ -1,12 +1,27 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from .auth import UserManager
+from django.core.validators import RegexValidator
 
 # Create your models here.
 
-class User(models.Model):
+class User(AbstractBaseUser):
+	username = models.CharField(max_length=50, unique=True)
 	first_name = models.CharField(max_length=50)
 	last_name = models.CharField(max_length=50)
+	email = models.EmailField(unique=True)
+
+	REQUIRED_FIELDS = ['username']
+
+	objects = UserManager()
+
+	def get_full_name(self):
+		return ''.join([self.first_name, self.last_name])
+
+	def get_short_name(self):
+		return self.first_name
 
 class Venue(models.Model):
 	venue_name = models.CharField(max_length=50)
@@ -18,4 +33,3 @@ class Event(models.Model):
 	Venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
 	Date = models.DateField(blank=True)
 	Time = models.TimeField(blank=True)
-
